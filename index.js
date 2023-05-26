b5d8d53b32d9ee11e49cc5780d2fb86923110902
ewagorka <ewagorka99@gmail.com>
2023-05-26 18:50:13 +0100

    added questions, and display them on app start

--- index.js
+++ index.js
@@ -5,6 +5,42 @@ const generateMarkdown = require("./utils/generateMarkdown");
 
 // array of questions for user
 const questions = [
+    {
+        type: 'input',
+        message: 'What is the title of your project?',
+        name: 'title',
+    },
+    {
+        type: 'input',
+        message: 'What iare the installation instructions?',
+        name: 'description',
+    },
+    {
+        type: 'input',
+        message: 'What languages do you know?',
+        name: 'instalation',
+    },
+    {
+        type: 'input',
+        message: 'What is the usage?',
+        name: 'usage',
+    },
+    {
+        type: 'checkbox',
+        message: 'What is the license of this project?',
+        name: 'license',
+        choices: ['Apache License 2.0','GNU GPLv3', 'MIT', 'ISC License'],
+    },
+    {
+        type: 'input',
+        message: 'Provide examples on how to run tests of your application:',
+        name: 'tests',
+    },
+    {
+        type: 'input',
+        message: 'Provide examples on how to run tests of your application:',
+        name: 'tests',
+    },
 
 ];
 
@@ -15,6 +51,13 @@ function writeToFile(fileName, data) {
 // function to initialize program
 function init() {
     console.log("Hello");
+    inquirer
+        // ask user all questions from the questions array
+        .prompt(questions)
+        // display answers
+        .then((data)=>{
+        console.log( JSON.stringify(data));
+    })
 }
 
 // function call to initialize program
